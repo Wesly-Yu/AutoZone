b5bb837aca6dd8f0d0b0ebc98a6df4a68267cbef
Wesly-Yu <1633235633@qq.com>
2019-05-23 21:38:56 +0800

    update--email-001

--- AutoZone/settings.py
+++ AutoZone/settings.py
@@ -146,3 +146,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),
 
 BROKER_URL = 'redis://127.0.0.1:6379/0'
 BROKER_TRANSPORT = 'redis'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = False   #是否使用TLS安全传输协议(用于在两个通信应用程序之间提供保密性和数据完整性。)
+EMAIL_USE_SSL = True    #是否使用SSL加密，qq企业邮箱要求使用
+EMAIL_HOST = 'smtp.qq.com'   #发送邮件的邮箱 的 SMTP服务器，这里用了163邮箱
+EMAIL_PORT = 25     #发件箱的SMTP服务器端口
+EMAIL_HOST_USER = '1633235633@qq.com'    #发送邮件的邮箱地址
+EMAIL_HOST_PASSWORD = '*********'         #发送邮件的邮箱密码(这里使用的是授权码)

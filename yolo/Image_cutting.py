3ba6f7b1e10468c5750329c3ee7364cfbb3a3a49
Wesley-Yu <1633235633@qq.com>
2019-11-01 17:55:27 +0800

    opencv 文字区域检测--004

--- yolo/Image_cutting.py
+++ yolo/Image_cutting.py
@@ -3,10 +3,8 @@ from PIL import Image
 import cv2
 import numpy as np
 import sys
-sys.path.append('/seglink/util')
-import tensorflow as tf
-from yolo import model
-import os
+
+
 def preprocess(gray):
     sobel = cv2.Sobel(gray,cv2.CV_8U,1,0,ksize =3)  #sober算子，x方向求梯度
     # Sobel 边缘检测二值化 类似PS中的图像颜色反转
@@ -64,64 +62,63 @@ def detect_image(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
-def text_detect(img):
-    # 模型路径
-    checkpoint_path = r'E:/EAST-master/east_icdar2015_resnet_v1_50_rbox/'
-
-    # 模型参数
-    input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
-    global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
-
-    f_score, f_geometry = model.model(input_images, is_training=False)
-
-    variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
-    saver = tf.train.Saver(variable_averages.variables_to_restore())
-
-    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-
-    # 加载模型
-    ckpt_state = tf.train.get_checkpoint_state(checkpoint_path)
-    model_path = os.path.join(checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
-    saver.restore(sess, model_path)
-
-    # 预测文本框
-    im_resized, (ratio_h, ratio_w) = resize_image(img)
-    score, geometry = sess.run(
-        [f_score, f_geometry],
-        feed_dict={input_images: [im_resized[:, :, ::-1]]})
-
-    boxes, _ = detect(score_map=score, geo_map=geometry,
-                      timer=collections.OrderedDict([('net', 0), ('restore', 0), ('nms', 0)]))
-
-    if boxes is not None:
-        scores = boxes[:, 8].reshape(-1)
-        boxes = boxes[:, :8].reshape((-1, 4, 2))
-        boxes[:, :, 0] /= ratio_w
-        boxes[:, :, 1] /= ratio_h
-
-    text_lines = []
-    if boxes is not None:
-        text_lines = []
-        for box, score in zip(boxes, scores):
-            box = sort_poly(box.astype(np.int32))
-            if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
-                continue
-            tl = collections.OrderedDict(zip(
-                ['x0', 'y0', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3'],
-                map(float, box.flatten())))
-            tl['score'] = float(score)
-            text_lines.append(tl)
-    ret = {
-        'text_lines': text_lines,
-    }
-    return ret
+#
+# def text_detect(img):
+#     # 模型路径
+#     checkpoint_path = r'E:/EAST-master/east_icdar2015_resnet_v1_50_rbox/'
+#
+#     # 模型参数
+#     input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
+#     global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+#
+#     f_score, f_geometry = model.model(input_images, is_training=False)
+#
+#     variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
+#     saver = tf.train.Saver(variable_averages.variables_to_restore())
+#
+#     sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+#
+#     # 加载模型
+#     ckpt_state = tf.train.get_checkpoint_state(checkpoint_path)
+#     model_path = os.path.join(checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
+#     saver.restore(sess, model_path)
+#
+#     # 预测文本框
+#     im_resized, (ratio_h, ratio_w) = resize_image(img)
+#     score, geometry = sess.run(
+#         [f_score, f_geometry],
+#         feed_dict={input_images: [im_resized[:, :, ::-1]]})
+#
+#     boxes, _ = detect(score_map=score, geo_map=geometry,
+#                       timer=collections.OrderedDict([('net', 0), ('restore', 0), ('nms', 0)]))
+#
+#     if boxes is not None:
+#         scores = boxes[:, 8].reshape(-1)
+#         boxes = boxes[:, :8].reshape((-1, 4, 2))
+#         boxes[:, :, 0] /= ratio_w
+#         boxes[:, :, 1] /= ratio_h
+#
+#     text_lines = []
+#     if boxes is not None:
+#         text_lines = []
+#         for box, score in zip(boxes, scores):
+#             box = sort_poly(box.astype(np.int32))
+#             if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
+#                 continue
+#             tl = collections.OrderedDict(zip(
+#                 ['x0', 'y0', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3'],
+#                 map(float, box.flatten())))
+#             tl['score'] = float(score)
+#             text_lines.append(tl)
+#     ret = {
+#         'text_lines': text_lines,
+#     }
+#     return ret
 
 
 
 
 if __name__ == '__main__':
-    imgpath = r'D:\AutoZone\yolo\test1.jpg'
+    imgpath = r'D:\AutoZone\yolo\test4.jpg'
     img=cv2.imread(imgpath)
-    text_detect(img)
-    # detect_image(img)
+    detect_image(img)
